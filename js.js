7bd94fa8db63eac9d58a0a36319fb7e8e6c35453
Caneesha <sophie.neufahrt@t-online.de>
2025-06-22 12:11:06 +0200

    Add & test operate function

--- js.js
+++ js.js
@@ -1,5 +1,9 @@
+let num1;
+let num2;
+let operator;
+
 function add (num1, num2) {
-    return num1 + num2;
+    console.log(num1 + num2);
 }
 
 function subtract (num1, num2) {
@@ -14,6 +18,24 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
-let num1;
-let num2;
-let operator;
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case "+" :
+            add (num1, num2);
+            break;
+        case "-" :
+            subtract (num1, num2);
+            break;
+        case "*" :
+            multiply (num1, num2);
+            break;
+        case "/" :
+            divide (num1, num2);
+            break;
+        default:
+            console.log(`Operate-switch-statement did'nt work. Operator is ${operator}.`);
+            
+    }
+}
+
+//test(works):operate (2, 3, "+");
