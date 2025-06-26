fab835c7640f996f7eaaa0fd4582968a0cef5942
Caneesha <sophie.neufahrt@t-online.de>
2025-06-26 11:43:30 +0200

    Add and test button ids & query selectors

--- js.js
+++ js.js
@@ -1,8 +1,32 @@
 const display = document.querySelector(".display");
 
+const button0 = document.querySelector("#button0");
+
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const button4 = document.querySelector("#button4");
+const button5 = document.querySelector("#button5");
+const button6 = document.querySelector("#button6");
+const button7 = document.querySelector("#button7");
+const button8 = document.querySelector("#button8");
+const button9 = document.querySelector("#button9");
+
+const buttonAdd = document.querySelector("#buttonAdd");
+const buttonSub = document.querySelector("#buttonSub");
+const buttonMult = document.querySelector("#buttonMult");
+const buttonDiv = document.querySelector("#buttonDiv");
+const buttonCalc = document.querySelector("#buttonCalc");
+
+const buttonC = document.querySelector("#buttonC");
+
+
 let num1;
 let num2;
 let operator;
+let displayContent;
+
 
 function add (num1, num2) {
     console.log(num1 + num2);
@@ -42,9 +66,8 @@ function operate (num1, num2, operator) {
 //test of operate-function(works): operate (2, 3, "+");
 
 
-let displayContent;
-
 function fillDisplay (displayContent) {
     display.textContent = displayContent;
 }
 //test function fillDisplay(works): fillDisplay(displayContent);
+
